b2e8ca356cfd9adb24ae3e6f46584364556b66e0
Cathy Richardson <cathyanne08@gmail.com>
2021-08-26 21:47:21 -0600

    completed step1

---
+++ server/controllers/authController.js
@@ -0,0 +1,26 @@
+const bcrypt = require('bcryptjs');
+
+const register = async (req, res) => {
+    const { username, password, isAdmin } = req.body;
+    const db = req.app.get('db');
+    const result = await db.get_user(username);
+    existingUser = result[0];
+    if (existingUser) {
+        res.status(409).send('Username taken');
+    } else {
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(password, salt);
+        const registeredUser = await db.register_user(isAdmin, username, hash);
+        const user = registeredUser[0];
+        req.session.user = {
+            isAdmin: user.is_admin,
+            id: user.id,
+            username: user.username
+        }
+        res.status(201).send(user);
+    }
+}
+
+module.exports = {
+    register
+}
--- server/index.js
+++ server/index.js
@@ -2,11 +2,31 @@ const express = require('express');
 const session = require('express-session');
 const massive = require('massive');
 require('dotenv').config();
+const authCtrl = require('./controllers/authController')
 
+const { SESSION_SECRET, CONNECTION_STRING } = process.env;
 const PORT = 4000;
 const app = express();
 
-
 app.use(express.json());
+app.use(
+    session({
+        resave: true,
+        saveUninitialized: false,
+        secret: SESSION_SECRET
+    })
+);
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: { rejectUnauthorized: false }
+}).then((dbInstance) => {
+    app.set("db", dbInstance);
+    console.log('db connected');
+}).catch(e => console.log(e));
+
+
+app.post('/auth/register', authCtrl.register);
+
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
