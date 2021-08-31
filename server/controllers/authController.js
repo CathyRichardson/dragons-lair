f2efbcb16b2a69c1611ad967f768881c3fd49c57
Cathy Richardson <cathyanne08@gmail.com>
2021-08-30 21:20:53 -0600

    completed step 2

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -4,12 +4,13 @@ const register = async (req, res) => {
     const { username, password, isAdmin } = req.body;
     const db = req.app.get('db');
     const result = await db.get_user(username);
-    existingUser = result[0];
+    const existingUser = result[0];
     if (existingUser) {
         res.status(409).send('Username taken');
     } else {
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(password, salt);
+        // const hash = bcrypt.hashSync(password); -- This line is equivalent to the two above
         const registeredUser = await db.register_user(isAdmin, username, hash);
         const user = registeredUser[0];
         req.session.user = {
